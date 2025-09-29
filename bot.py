c9cefa49b97e503e12eadfeffee07fd0511c46b7
Anton Satt <makeorbreak+github@pm.me>
2025-09-29 19:41:25 +0200

    testing openrouter

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import os
 import discord
 from dotenv import load_dotenv
+import requests
 
 # Load environment variables from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+AI_API_TOKEN = os.getenv('OPENROUTER_API_KEY')
 
 # Create an instance of the bot with command prefix
 intents = discord.Intents.default()
@@ -24,12 +26,43 @@ async def on_message(message):
     if message.content.startswith('!'):
         command = message.content.split()[0][1:]
         if command == 'help':
-            await message.channel.send('Current commands: !help, !ping')
+            await message.channel.send('Current commands: !help, !ping, !meme')
         elif command == 'ping':
             await message.channel.send('PongPongBong!')
         elif command == 'meme':
             await message.channel.send('https://i.imgur.com/ljHAAuL.png')
-    
+    if message.content.startswith('roastme'):
+        response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {AI_API_TOKEN}",
+            "Content-Type": "application/json",
+        },
+        json =({
+        "model": "x-ai/grok-4-fast:free",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+            {
+                "type": "text",
+                "text": "What is in this image?"
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+                }
+            }
+            ]
+        }
+        ],
+        
+    })
+    )
+
+
+
 
 # Run the bot wi
 # th your token
